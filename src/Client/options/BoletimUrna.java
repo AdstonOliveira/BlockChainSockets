c90eb49c0b5f8c340cd879dd776b691a64d07d0d
adston oliveira <suportetecnico.adston@gmail.com>
2019-06-17 12:32:20 -0300

    Fatoração de itens. Add PDF e XML

--- src/Client/options/BoletimUrna.java
+++ src/Client/options/BoletimUrna.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Client.options;
-
 /**
- *
  * @author Suporte-04
  */
 public class BoletimUrna {
+
+    //Conteudo completo
+
+
     
 }
